f4e57ff40d9230bd7b2039756833d93746cd254b
Víctor Gutiérrez <victor.gutierrez@i3television.es>
2019-01-23 17:12:35 +0100

    First style tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
 /** antd */
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button>Ver listas</Button>
-        <Button>Crear listas</Button>
-        <Button>Ajustes</Button>
-      </div>
+      <Row gutter={24} type="flex" justify="center">
+        <Col span={6} offset={3}>
+          <Button>Ver listas</Button>
+        </Col>
+        <Col span={6}>
+          <Button>Crear listas</Button>
+        </Col>
+        <Col span={6}>
+          <Button>Ajustes</Button>
+        </Col>
+      </Row>
     );
   }
 }
