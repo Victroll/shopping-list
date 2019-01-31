a7fba71194b4d3804b95ac103f0e7145b91877d4
Victroll <victorgutierrez1988@gmail.com>
2019-01-31 12:05:38 +0100

    Fixed problem with ReactRouter and base url

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => (
     <Layout>
       <GitHubCorner />
       <Content className='content'>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route
               exact
